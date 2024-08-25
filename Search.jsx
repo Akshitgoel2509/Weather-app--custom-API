379a6c7a952dac8ff13124c3393f3ba03651886b
Akshit Goel <97082326+Akshitgoel2509@users.noreply.github.com>
2024-08-25 15:39:33 +0530

    Update Search.jsx

--- Search.jsx
+++ Search.jsx
@@ -10,15 +10,14 @@ import  rain from "./assets/rain.png";
  import drizzle from "./assets/drizzle.png"
  import humidity from "./assets/humidity.png"
  import snow from "./assets/snow.png"
-//  import { useRef } from 'react';
+
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 
 
 const Search = () => {
     const {query,searchPost,weather,main,coord}=useGlobalContext();
     const [value,setvalue]=useState("");
-    // const ref=useRef()
-    // let str=weather[0].description.replace(/"/g,"");
+    
     let sky="";
     if(weather[0].description.includes("clouds") ){
       sky=clouds;
@@ -43,42 +42,21 @@ const Search = () => {
     <>
      {/* left conatainer */}
      <div className="leftContainer bg-white " style={{"width":"35%","overflowY":"auto","height":"70vh" }}>
-    
-     {/* <div className="container-fluid">
-    <form className="d-flex" onSubmit={e=>e.preventDefault()}>
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={e=>searchPost(e.target.value)}/>
-      <i className="fas fa-search "></i>
-    </form>
-  </div> */}
+   
   <div className='mt-4 ms-3 text-center '>
        <form onSubmit={e=>{e.preventDefault()}} > 
         <div >
           <input
             className='rounded-pill w-75 ps-4 '
             type="search"
-            placeholder="Ex:- London"
+            placeholder="Ex:- Delhi"
 
             value={value}
             onChange={(e)=>{setvalue(e.target.value)}}
           />
-          {/* <i className="">{TravelExploreIcon}</i> */}
           <button onClick={()=> searchPost(value)} className="btn btn-outline-primary rounded-pill mx-1 fw-bold ">Search</button>
           
-          {/* <select name="place" id="place" value={query} onChange={(e)=>{searchPost(e.target.value)}} >
-            <option value="delhi">delhi</option>
-            <option value="mumbai">Mumbai</option>
-            <option value="kolkata">Kolkata</option>
-            <option value="dubai">dubai</option>
-            <option  value="london">london</option>
-            <option  value="san francisco">san francisco</option>
-            <option  value="melbourne">melbourne</option>
-            <option  value="shanghai">shanghai</option>
-            <option  value="japan">japan</option>
-            <option  value="prague">prague</option>
-            <option  value="maldives">maldives</option>
-            <option value="adarsh nagar">adarsh nagar</option>
-          </select> */}
-          {/* <input type="submit" value="submit" onClick={(e)=>{searchPost(e.target.value)}} /> */}
+
           
         </div>
       </form>
