dfa265db3582dd3c2518613bdf247d80605e8b51
Akshit Goel <97082326+Akshitgoel2509@users.noreply.github.com>
2024-08-25 22:41:40 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -18,7 +18,7 @@ function App() {
 
      {/* <div className="full-page " style={{"height":"100vh","backgroundColor":"#cfcfcf"}}> */}
      <div className="full-page " style={{"height":"100vh","backgroundImage":`url(https://t3.ftcdn.net/jpg/05/80/90/24/360_F_580902442_xmjBIVys6czucJ4T8JgbSxh6h7EAAn1P.jpg)`, }}>
-     <div className=" text-center fs-1 fw-bold mb-2 py-4 text-white "> Weather App</div>
+     <div className=" text-center fs-1 fw-bold mb-2 py-4 text-white "> WeatherFry</div>
 
      {/* main container */}
      <div className="container d-flex" >
